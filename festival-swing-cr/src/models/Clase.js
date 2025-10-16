0aace52ad8189d605fe37c2e08b8936b334dd854
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-10-16 20:22:34 +0200

    Refactor(models): modificación de las clases
    He modificado los atributos y he eliminado la clase sala y profesor para el buen funcionamiento de la página.

--- festival-swing-cr/src/models/Clase.js
+++ festival-swing-cr/src/models/Clase.js
@@ -1,10 +1,8 @@
 import { Evento } from './Evento.js';
 
 export class Clase extends Evento {
-  constructor({ nivel, estilo, profesores = [], ...eventoAtributos }) {
-    super(eventoAtributos); // id, nombre, descripcion, salaId, dia, hora, duracion
-    this.nivel = nivel;   // básico, intermedio, avanzado
-    this.estilo = estilo; // Lindy Hop, Solo Jazz, etc
-    this.profesores = profesores; // array de objetos
+  constructor({ nivel, ...eventoAtributos }) {
+    super(eventoAtributos);
+    this.nivel = nivel;                   
   }
 }
